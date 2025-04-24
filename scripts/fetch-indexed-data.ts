bbe6d220c8d55a886b45fc7755669f0e38534a9f
albertov19 <albertoviera@gmail.com>
2025-04-24 14:28:16 +0200

    first commit

---
+++ scripts/fetch-indexed-data.ts
@@ -0,0 +1,146 @@
+// @ts-ignore
+const fetch = require('node-fetch');
+
+// Dancebox data block 6801516
+const danceboxTx = BigInt('27920878'); // 27187292 system + 733586 signed https://dancebox.subscan.io/extrinsic?page=1&time_dimension=date&signed=all
+const danceboxAccounts = BigInt('35718'); // From Subscan https://dancebox.subscan.io/
+
+// GraphQL endpoint for the Tanssi campaign
+const GRAPHQL_URL_APPCHAINS_DEPLOYMENT =
+  'https://tanssi-campaign.squids.live/flashbox-appchain-deployment/graphql';
+
+const GRAPHQL_URL_APPCHAINS_DATA =
+  'https://tanssi-campaign.squids.live/airlyft/graphql';
+
+// Queries
+const queryAppchains = `
+  query {
+    parachainIdRegistrations {
+      paraId
+    }
+  }
+`;
+
+const queryTxAndAddresses = `
+  query MyQuery {
+    transactionsConnection(orderBy: id_ASC) {
+      totalCount
+    }
+    addressesConnection(orderBy: id_ASC) {
+      totalCount
+    }
+  }
+`;
+
+interface AppchainRegistration {
+  paraId: number;
+}
+
+interface RegisteredAppchainsInterface {
+  data?: {
+    parachainIdRegistrations: AppchainRegistration[];
+  };
+  errors?: any;
+}
+
+interface TotalTxAndAddressesInterface {
+  data?: {
+    transactionsConnection: {
+      totalCount: number;
+    };
+    addressesConnection: {
+      totalCount: number;
+    };
+  };
+  errors?: any;
+}
+
+async function main() {
+  // Appchains deployment
+  try {
+    const response = await fetch(GRAPHQL_URL_APPCHAINS_DEPLOYMENT, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: queryAppchains }),
+    });
+
+    const result: RegisteredAppchainsInterface = await response.json();
+
+    if (result.errors) {
+      console.error('GraphQL errors:', result.errors);
+      return;
+    }
+
+    const registrations = result.data?.parachainIdRegistrations;
+
+    // Get actual deployments vs intents
+    const appchainIntents = registrations.filter(
+      (item) => item.paraId === 0
+    ).length;
+    const appchainDeployments = registrations.filter(
+      (item) => item.paraId !== 0
+    ).length;
+
+    if (registrations) {
+      console.log(
+        'Number of total Appchains:',
+        (BigInt(appchainDeployments) + BigInt(appchainIntents)).toString()
+      );
+      console.log(
+        '------> Appchains deployed:',
+        appchainDeployments.toString()
+      );
+      console.log('------> Appchains intents:', appchainIntents.toString());
+    } else {
+      console.log('No data received.');
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+
+  // Appchains data
+  try {
+    const response = await fetch(GRAPHQL_URL_APPCHAINS_DATA, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: queryTxAndAddresses }),
+    });
+
+    const result: TotalTxAndAddressesInterface = await response.json();
+
+    if (result.errors) {
+      console.error('GraphQL errors:', result.errors);
+      return;
+    }
+
+    const totalTransactions = result.data?.transactionsConnection.totalCount;
+    const totalAddresses = result.data?.addressesConnection.totalCount;
+
+    if (totalTransactions && totalAddresses) {
+      console.log(
+        'Number of total addresses:',
+        (BigInt(totalAddresses) + danceboxAccounts).toString()
+      );
+      console.log('------> Dancebox addresses:', danceboxAccounts.toString());
+      console.log(
+        '------> Flashbox Appchains addresses:',
+        totalAddresses.toString()
+      );
+      console.log(
+        'Number of transactions:',
+        (BigInt(totalTransactions) + danceboxTx).toString()
+      );
+      console.log('------> Dancebox transactions:', danceboxTx.toString());
+      console.log(
+        '------> Flashbox Appchains transactions:',
+        totalTransactions.toString()
+      );
+    } else {
+      console.log('No data received.');
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+main();
